246da4d7b35a8c0b790b547477002773b8128950
naghtigal <hatanm@gmail.com>
2018-03-07 21:41:23 +0200

    [client]: added application layout

--- src/App.js
+++ src/App.js
@@ -1,21 +1,58 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            result: 'H',
+            head: 2,
+            tail: 0,
+            balance: 30,
+            win: 1,
+            round: 2,
+        }
+    }
+
+    render () {
+        return (
+            <div className="App">
+                <div className="App-result-row">
+                    <div/>
+                    <div>{this.state.result}</div>
+                    <div/>
+                </div>
+                <div className="App-bet-row">
+                    <div className="App-bet-head">
+                        <div>Head</div>
+                        <div>{this.state.head}</div>
+                    </div>
+                    <div className="App-bet-tail">
+                        <div>Tail</div>
+                        <div>{this.state.tail}</div>
+                    </div>
+                </div>
+                <div className="App-control-row">
+                    <div className="App-balance">
+                        <div>Balance</div>
+                        <div>{this.state.balance}</div>
+                    </div>
+                    <div className="App-flip">
+                        <button>Flip!</button>
+                    </div>
+                    <div className="App-win">
+                        <div>Win</div>
+                        <div>{this.state.win}</div>
+                    </div>
+                    <div className="App-round">
+                        <div>Round</div>
+                        <div>{this.state.round}</div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
